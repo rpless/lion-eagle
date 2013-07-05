500c90df16a94716727e85da1916b7e33865d219
Ryan Plessner <rpless@ccs.neu.edu>
2013-07-05 18:12:05 -0400

    added the ability to use a message in the view

--- example.rkt
+++ example.rkt
@@ -1,9 +1,11 @@
 #lang racket
-(require "mvc.rkt" racket/gui/base)
+(require "mvc.rkt")
 
 (define-values (x view)
   (mvc (model foo [counter 0])
        (view (frame "MVC Test"
+                    (message m1 (bind counter number->string))
                     (textfield t1 (bind counter number->string string->number))
                     (button b1 "Click Me" (action hello))))
        (controller (hello (send this set-counter (add1 (send this get-counter)))))))
+(send view show #t)
--- view.rkt
+++ view.rkt
@@ -13,20 +13,28 @@
 ;;   The name is the id of the textfield, the field is the field in the model it is bound to
 ;;   The model->textfield is a function of [X -> String] and the textfield->model is a function
 ;;   of [String -> X]
+;; - (button name text (action actionname))
 (define-syntax (component stx)
-  (syntax-case stx (textfield button bind action)
+  (syntax-case stx (textfield button message bind action)
     [(_ control parentname (textfield name (bind field model->textfield textfield->model)))
      #`(begin 
-         (define name (new text-field% [label ""]
-                           [parent parentname]
+         (define name (new text-field% [parent parentname]
+                           [label ""]
                            [callback (λ (self evt) 
                                        (when (eq? (send evt get-event-type) 'text-field-enter)
-                                           (send control #,(datum->syntax stx (symbol-append 'set- (syntax->datum #'field)))
-                                                 (textfield->model (send self get-value)))))]))
+                                         (send control #,(datum->syntax stx (symbol-append 'set- (syntax->datum #'field)))
+                                               (textfield->model (send self get-value)))))]))
          (send control #,(datum->syntax stx (symbol-append 'add-notifer: (syntax->datum #'field)))
                (λ (new-value) (send name set-value (model->textfield new-value)))))]
+    [(_ control parentname (message name (bind field model->message)))
+     #`(begin 
+         (define name (new message% [parent parentname]
+                           [label (model->message (send control #,(datum->syntax stx (symbol-append 'get- (syntax->datum #'field)))))]
+                           [auto-resize #t]))
+         (send control #,(datum->syntax stx (symbol-append 'add-notifer: (syntax->datum #'field)))
+               (λ (new-value) (send name set-label (model->message new-value)))))]
     [(_ control parentname (button name text (action actionname)))
-     #`(begin (define name (new button% [parent parentname]
-                                    [label text]
-                                    [callback (λ (self evt) (when (eq? (send evt get-event-type) 'button)
-                                                              (send control actionname)))])))]))
+     #`(define name (new button% [parent parentname]
+                         [label text]
+                         [callback (λ (self evt) (when (eq? (send evt get-event-type) 'button)
+                                                   (send control actionname)))]))]))
