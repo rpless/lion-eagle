fe6831ce15b172e87a778efa433c6ef1f0daa939
Ryan Plessner <rpless@ccs.neu.edu>
2013-08-17 15:37:24 -0400

    added a component module. implemented message creation in it

--- lion-eagle/view.rkt
+++ lion-eagle/view.rkt
@@ -1,42 +1,19 @@
 #lang racket
 
-(require (for-syntax racket "private/utilities.rkt")
-         racket/gui/base)
+(require (for-syntax racket)
+         "private/component.rkt" racket/gui/base)
 
 ;; View Module
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide define-view)
 
-(define-syntax (define-view stx) 
-  
-  (define ->syntax (curry datum->syntax stx))
-  
-  (define (component comp parentname)
-      (syntax-case comp (bind textfield message)
-        [(textfield name (bind field model->field field->model))
-         #`(begin 
-             (define callback 
-               (λ (self evt)
-                 (when (eq? (send evt get-event-type) 'text-field-enter)
-                   (#,(->syntax (symbol-append 'set- (syntax->datum #'field) '!)) (field->model (send self get-value))))))
-             (define initial-value (#,(->syntax (symbol-append 'get- (syntax->datum #'field)))))
-             (define name (new text-field% [parent #,parentname][label ""][init-value (model->field initial-value)][callback callback]))
-             (#,(->syntax (symbol-append 'add-notifier: (syntax->datum #'field)))
-              (λ (new-value) (send name set-value (model->field new-value)))))]
-        [(message name (bind field model->message))
-         #`(begin 
-             (define name (new message% [parent #,parentname]
-                               [label (model->message (#,(->syntax (symbol-append 'get- (syntax->datum #'field)))))]
-                               [auto-resize #t]))
-             (#,(->syntax (symbol-append 'add-notifier: (syntax->datum #'field)))
-              (λ (new-value) (send name set-label (model->message new-value)))))]))
-  
+(define-syntax (define-view stx)   
   (syntax-case stx (frame)
     [(_ (frame parentname title comp ...))
      #`(let ()
          (define parentname (new frame% [label title]))
-         #,@(map (curryr component #'parentname) (syntax->list #'(comp ...)))
+         #,@(map (curryr component #'parentname stx) (syntax->list #'(comp ...)))
          (send parentname show #t))]))
 
 ;; The component form supports the following types of UI components:
