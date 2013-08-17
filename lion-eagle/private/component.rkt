e755e4cac0b6d2317f7b37145e9864be25331081
Ryan Plessner <rpless@ccs.neu.edu>
2013-08-17 15:51:21 -0400

    added textfields to the component module

--- lion-eagle/private/component.rkt
+++ lion-eagle/private/component.rkt
@@ -15,21 +15,26 @@
   (define ->syntax (curry datum->syntax stx))
   (syntax-case comp (bind textfield message)
     [(textfield name (bind field model->field field->model))
-     #`(begin 
-         (define callback 
-           (λ (self evt)
-             (when (eq? (send evt get-event-type) 'text-field-enter)
-               (#,(->syntax (symbol-append 'set- (syntax->datum #'field) '!)) (field->model (send self get-value))))))
-         (define initial-value (#,(->syntax (symbol-append 'get- (syntax->datum #'field)))))
-         (define name (new text-field% [parent #,parentname][label ""][init-value (model->field initial-value)][callback callback]))
-         (#,(->syntax (symbol-append 'add-notifier: (syntax->datum #'field)))
-          (λ (new-value) (send name set-value (model->field new-value)))))]
+     (create-textfield #'name #'field parentname #'model->field #'field->model stx)]
     [(message name (bind field model->message))
      (create-message #'name #'field parentname #'model->message stx)]))
 
 
 ;; Gui Components
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-for-syntax (create-textfield name field parentname model->field field->model stx)
+  (define ->syntax (curry datum->syntax stx))
+  #`(begin 
+      (define callback 
+        (λ (self evt)
+          (when (eq? (send evt get-event-type) 'text-field-enter)
+            (#,(->syntax (symbol-append 'set- (syntax->datum field) '!)) (#,field->model (send self get-value))))))
+      (define initial-value (#,(->syntax (symbol-append 'get- (syntax->datum field)))))
+      (define #,name (new text-field% [parent #,parentname][label ""][init-value (#,model->field initial-value)][callback callback]))
+      (#,(->syntax (symbol-append 'add-notifier: (syntax->datum field)))
+       (λ (new-value) (send #,name set-value (#,model->field new-value))))))
+
 
 (define-for-syntax (create-message name field parentname model->message stx)
   (define ->syntax (curry datum->syntax stx))
